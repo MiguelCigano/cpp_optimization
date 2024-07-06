0e6b4077ac403f44afc29dff0675ed1976b53c90
MiguelCigano <miguel_m0130@hotmail.com>
2024-07-06 01:42:49 -0700

    Se agrega los primeros archivos de este proyecto

---
+++ opt2d/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    std::cout << "Buenas noches Miguel" << std::endl;
+    std::cout << "Buenas noches de nuevo" << std::endl;
+    return 0;
+}
