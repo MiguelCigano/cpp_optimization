3b34125fe7fb23c5d431c8132061580f92a402ef
MiguelCigano <miguel_m0130@hotmail.com>
2024-07-06 02:33:58 -0700

    se agrega la función de matriz inversa

--- opt2d/linalg/linalg.cpp
+++ opt2d/linalg/linalg.cpp
@@ -20,5 +20,30 @@ void imprimeMatriz(Matrix x)
 
 double Norma(Matrix x)
 {
-    
+    double x1 = x.getValue(0,0);
+    double x2 = x.getValue(0,1);
+
+    return sqrt(pow(x1, 2) + pow(x2, 2));
 }
+
+Matrix invertidaMatriz(Matrix x)
+{
+    double a = x.getValue(0,0);
+    double b = x.getValue(0,1);
+    double c = x.getValue(1,0);
+    double d = x.getValue(1,1);
+
+    Matrix y({{d,-b}, {-c,a}});
+    double determinante = 1/(a*d -b*c);
+
+    for(int i = 0; i < y.Rows(); i++)
+    {
+        for(int j = 0; i < y.Columns; j++)
+        {
+            y.setValue(i,j, determinante*y.getValu(i,j));
+        }
+    }
+    return y;
+}
+
+
